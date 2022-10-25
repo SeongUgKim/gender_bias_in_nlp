e059b4ff8fded8d7225290b65666abe6b0a903e3
Seong Ug Kim <sukim2017@gmail.com>
2022-10-25 13:23:15 -0400

    seongk/eval

---
+++ eval.py
@@ -0,0 +1,134 @@
+import argparse
+import csv
+
+import spacy
+import pandas as pd
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--lang', type=str, required=True,
+                        choices=['en', 'de', 'ja', 'ar', 'es', 'pt', 'ru', 'id', 'zh'],
+                        help='Path to evaluation dataset.')
+    parser.add_argument('--male_words', type=str, required=True)
+    parser.add_argument('--female_words', type=str, required=True)
+    parser.add_argument('--corpus', type=str, required=True)
+    parser.add_argument('--k', type=int, required=True)
+    args = parser.parse_args()
+    return args
+
+
+def tokenize(src, lang, k):
+    tokenizer = None
+    ret = []
+    count = 0
+    talks = []
+    if lang == 'en':
+        tokenizer = 'en_core_web_sm'
+    elif lang == 'de':
+        tokenizer = 'de_core_news_sm'
+    elif lang == 'ja':
+        tokenizer = 'ja_core_news_sm'
+    elif lang == 'ar':
+        tokenizer = 'xx_ent_wiki_sm'
+    elif lang == 'es':
+        tokenizer = 'es_core_news_sm'
+    elif lang == 'pt':
+        tokenizer = 'pt_core_news_sm'
+    elif lang == 'ru':
+        tokenizer = 'ru_core_news_sm'
+    elif lang == 'id':
+        tokenizer = 'xx_ent_wiki_sm'
+    elif lang == 'zh':
+        tokenizer = 'zh_core_web_sm'
+    word_tokenizer = spacy.load(tokenizer)
+    for i in range(src.shape[0]):
+        temp = [token.text.lower() for token in word_tokenizer(src[lang][i])]
+        # if i > 0 and src['talkid'][i - 1] != src['talkid'][i] and count >= 2 * k + 1:
+        #     ret.extend(talks)
+        #     talks = []
+        #     count = 0
+        # else:
+        #     talks.append((src['talkid'][i], src[lang][i], temp))
+        ret.append((src['talkid'][i], src[lang][i], temp, i))
+    return ret
+
+
+def get_index(tokens_list, index, k):
+    minus = []
+    plus = []
+    current = []
+    ret = []
+    for i in reversed(range(1, k + 1)):
+        if index - i < 0 or tokens_list[index - i][0] != tokens_list[index][0]:
+            continue
+        minus.append(index - i)
+    # masking
+    current.append(index)
+    for i in range(1, k + 1):
+        if index + i >= len(tokens_list) or tokens_list[index + i][0] != tokens_list[index][0]:
+            continue
+        plus.append(index + i)
+    if len(minus) < k:
+        for i in range(1, k - len(minus) + 1):
+            plus.append(index + k + i)
+    if len(plus) < k:
+        for i in range(1, k - len(plus) + 1):
+            minus.insert(0, index - (k + i))
+    ret.extend(minus)
+    ret.extend(current)
+    ret.extend(plus)
+    return ret
+
+
+def extract_kth_neighbor_sentences(tokens_list, male_words, female_words, k):
+    output = pd.DataFrame(columns=['sentence'])
+    for i in range(len(tokens_list)):
+        male = [word for word in tokens_list[i][2] if word in male_words]
+        female = [word for word in tokens_list[i][2] if word in female_words]
+        if (len(male) == 1 and len(female) == 0) or (len(male) == 0 and len(female) == 1):
+            sentence = ''
+            indices = get_index(tokens_list, i, k)
+            cnt = 0
+            for index in indices:
+                if i == index:
+                    cnt += len(tokens_list[index][2])
+                    temp = tokens_list[index][1]
+                    temp_word = male[0] if len(male) == 1 else female[0]
+                    mask_index = temp.find(temp_word)
+                    result = temp[:mask_index] + '[MASK]' + temp[mask_index + len(temp_word):]
+                    sentence = sentence + (result + ' ')
+                else:
+                    sentence = sentence + (tokens_list[index][1] + ' ')
+            if cnt > 512:
+                continue
+            output = pd.concat([output, pd.DataFrame([sentence[:len(sentence) - 1]], columns=output.columns)],
+                               ignore_index=True)
+    return output
+
+
+def main(args):
+    lang = args.lang
+    male = args.male_words
+    female = args.female_words
+    corpus = args.corpus
+    k = args.k
+    ted = pd.read_csv(corpus, sep='\t', keep_default_na=False,
+                      encoding='utf8', quoting=csv.QUOTE_NONE)
+    if lang == 'zh':
+        ted_lang = ted[['talkid', 'zh-cn']]
+    else:
+        ted_lang = ted[['talkid', lang]]
+    with open(male) as f:
+        male_words = [line[:len(line) - 1] for line in f.readlines()]
+    with open(female) as f:
+        female_words = [line[:len(line) - 1] for line in f.readlines()]
+    tokens_list = tokenize(ted_lang, lang, k)
+    gender_sentence = extract_kth_neighbor_sentences(tokens_list, male_words, female_words, k)
+    print(gender_sentence['sentence'][0])
+    return
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    main(args)
